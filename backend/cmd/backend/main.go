eb8a9bc13d93f7476f0d8d4413c6c2d74597170e
rudraksh121a <rudrakshkanungo2022@gmail.com>
2026-01-22 00:12:36 +0530

    graceful shutdown

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Rudraksh121a/BookStore/internal/config"
 )
@@ -29,10 +34,22 @@ func main() {
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
-	fmt.Println("server started")
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("unable to start a server")
-	}
+	slog.Info("server started", slog.String("Address", cfg.Addr))
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+	<-done
 
+	slog.Info("shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+	slog.Info("Server Shutdown successfully")
 }
